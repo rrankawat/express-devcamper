05faae8d8e179194533a32177d59d48a217ad548
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2021-05-26 00:05:09 +0530

    Reset Password

--- routes/auth.js
+++ routes/auth.js
@@ -7,11 +7,13 @@ const {
   login,
   getMe,
   forgotPassword,
+  resetPassword,
 } = require('../controllers/auth');
 
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/me').get(protect, getMe);
 router.route('/forgotpassword').post(forgotPassword);
+router.route('/resetPassword/:resettoken').put(resetPassword);
 
 module.exports = router;
